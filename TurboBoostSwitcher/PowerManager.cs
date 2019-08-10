207b29cfbbc911281c254e3ff71c0c6b58440b78
SDchao <1511047037@qq.com>
2019-08-10 16:33:19 +0800

    生成项目

---
+++ TurboBoostSwitcher/PowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurboBoostSwitcher;
+
+namespace TurboBoostSwitcher
+{
+    class PowerManager
+    {
+        private string scheme_GUID;
+        private string sub_GUID;
+        private string setting_GUID;
+
+        public PowerManager()
+        {
+            CmdResult schemeListResult = CmdRunner.CmdRun("powercfg /l");
+            CmdResult queryResult = CmdRunner.CmdRun("powercfg /q");
+
+            //查找当前电源方案的GUID
+        }
+    }
+}
